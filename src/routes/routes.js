7e664a428e5d5574e03756f777f1fa4beab9965a
Atul Kale <atulk@axioned.com>
2023-03-11 21:42:50 +0530

    Updated route name form user to auth.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const authRoutes = require('./authRoutes');
 
-router.use('/user', userRoutes);
+router.use('/auth', authRoutes);
 
 module.exports = router;
