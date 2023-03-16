b2114325bd48f6c9e5ead4b3010d1338d1007443
Atul Kale <atulk@axioned.com>
2023-03-17 00:50:55 +0530

    Auth swagger documentation added.

--- app.js
+++ app.js
@@ -4,13 +4,34 @@ const bodyParser = require('body-parser');
 const dotenvFlow = require('dotenv-flow');
 const cookieParser = require('cookie-parser');
 const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 dotenvFlow.config();
 console.log(' Current Environment ===>', process.env.NODE_ENV);
 
 //Local Modules
-const swaggerDocument = require('./swagger.json');
 const routes = require('./src/routes/routes');
+
+const swaggerOptions = {
+  failOnErrors: true,
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'To-Do App Documentation',
+      version: '1.0.0',
+      description:
+        'This is a To-Do API application made with Express and MonodoDB where user can create and manage their tasks.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const app = express();
 
@@ -38,7 +59,7 @@ app.use(
 app.use('/api', routes);
 
 /* Swagger api-docs Routes */
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 /* Handling invalid route */
 app.use('/', function (req, res) {
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,264 @@
 const router = require('express').Router();
 const { register, login, verify } = require('../controllers/authController');
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *         password:
+ *           type: string
+ *           description: The password of the user
+ *       example:
+ *         email: kaleatul9@gmail.com
+ *         password: test
+ */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Result:
+ *      type: object
+ *      required:
+ *        - success
+ *        - error
+ *        - data
+ *      properties:
+ *        success:
+ *          type: boolean
+ *          description: The status of the request
+ *        error:
+ *          type: string
+ *          nullable: true
+ *          description: The error message of the request
+ *        data:
+ *          type: string
+ *          nullable: true
+ *          description: The response data of the request
+ */
+
+/**
+ * @swagger
+ * components:
+ *  securitySchemes:
+ *    cookieAuth:
+ *      type: apiKey
+ *      in: cookie
+ *      name: jwtToken
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ *  description: The users managing API
+ */
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *  post:
+ *    summary: Register a user and sent OTP to registered email id
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: A result object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: true
+ *              error: null
+ *              data: Register Successful
+ *      403:
+ *        description: Validation Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ *      409:
+ *        description: User already exists!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ *      500:
+ *        description: Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ */
+
 router.post('/register', register);
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *  post:
+ *    summary: Login a user and sent JWT token cookie with response
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    security: []
+ *    responses:
+ *      200:
+ *        description: A result object
+ *        headers:
+ *          Set-Cookie:
+ *            schema:
+ *              type: string
+ *              example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTA1ODA3NGU4YzkwMTYyYWQyYmJkMiIsImlhdCI6MTY3ODk5MjYwMiwiZXhwIjoxNjc4OTkyOTAyfQ.Y18fOrxOVeiMwHl9VDL6UJSdhiSToyxoNSlnKU2B2jY; Path=/; HttpOnly
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: true
+ *              error: null
+ *              data: Login Successful
+ *      403:
+ *        description: Validation Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ *      409:
+ *        description: User already exists!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ *      500:
+ *        description: Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ */
+
 router.post('/login', login);
+
+/**
+ * @swagger
+ * /api/auth/verify:
+ *  post:
+ *
+ *    summary: Verify the user with valid OTP
+ *    tags: [Users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              otp:
+ *                type: integer
+ *            example:
+ *              email: kaleatul9@gmail.com
+ *              otp: 123456
+ *    security:
+ *      - cookieAuth: []
+ *    responses:
+ *      200:
+ *        description: A result object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: true
+ *              error: null
+ *              data: Login Successful
+ *      401:
+ *        description: OTP did not match!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ *      403:
+ *        description: Validation Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ *      404:
+ *        description: User not found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ *      500:
+ *        description: Server Error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Result'
+ *            example:
+ *              success: false
+ *              error: Error massage
+ *              data: null
+ */
+
 router.post('/verify', verify);
 
 module.exports = router;
